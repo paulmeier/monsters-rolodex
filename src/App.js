d710c98c743dc549f6c5c3af3f31018ed3664651
Paul Meier <paulmartinmeier@gmail.com>
2021-02-21 10:33:42 -0600

    adding components

--- src/App.js
+++ src/App.js
@@ -1,29 +1,37 @@
 import { Component } from "react";
-import logo from "./logo.svg";
+import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
 import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      string: "Hello People!",
+      monsters: [],
+      searchField: "",
     };
   }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+  changeHandler = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{this.state.string}</p>
-          <button
-            onClick={() => {
-              console.log("HAHAHAH");
-              this.setState({ string: "Goodbye People!" });
-            }}
-          >
-            Change Text
-          </button>
-        </header>
+        <h1>Monsters Rolodex</h1>
+        <SearchBox
+          placeholder="search monsters"
+          changeHandler={this.changeHandler}
+        ></SearchBox>
+        <CardList monsters={filteredMonsters}></CardList>
       </div>
     );
   }
